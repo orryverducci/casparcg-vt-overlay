fd55e43ba846bcb39ac17b9d2ed981916f195c86
Orry Verducci <orry@orryverducci.co.uk>
2026-04-25 16:57:55 +0100

    Add OSC message events

--- src/caspar-osc.js
+++ src/caspar-osc.js
@@ -7,5 +7,20 @@ export default class CasparOsc {
 
     constructor(config) {
         this.#server = new Server(Config.oscPort, '0.0.0.0');
+
+        this.#server.on('message', this.processMessage.bind(this));
+        this.#server.on('bundle', this.processBundle.bind(this));
+    }
+
+    processBundle(bundle) {
+        Logger.debug('OSC bundle received');
+
+        bundle.elements.forEach((msg) => {
+            this.processMessage(msg).bind(this);
+        });
+    }
+
+    processMessage(msg) {
+        Logger.debug(`OSC message received: ${msg}`);
     }
 }
