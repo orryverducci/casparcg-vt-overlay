82ca35b91a92e5738133e6c5648551be21fd8d2d
Orry Verducci <orry@orryverducci.co.uk>
2026-04-26 11:46:49 +0100

    Add initial OSC parsing

--- src/caspar-osc.js
+++ src/caspar-osc.js
@@ -3,9 +3,12 @@ import Config from './config.js';
 import Logger from './logger.js';
 
 export default class CasparOsc {
+    #channels;
     #server;
 
     constructor(config) {
+        this.#channels = Config.channels;
+
         this.#server = new Server(Config.oscPort, '0.0.0.0');
 
         this.#server.on('message', this.processMessage.bind(this));
@@ -22,5 +25,24 @@ export default class CasparOsc {
 
     processMessage(msg) {
         Logger.debug(`OSC message received: ${msg}`);
+
+        // CasparCG sends time information on addresses in the format: /channel/1/stage/layer/1/file/time
+        if (!msg.address.endsWith("/file/time")) {
+            return;
+        }
+
+        let address = msg.address.split('/');
+        let addressChannelIndex = address.findIndex('channel');
+        let addressLayerIndex = address.findIndex('layer');
+
+        let channel = addressChannelIndex !== -1 ? parseInt(address[addressChannelIndex + 1]) : Number.NaN;
+        let layer = addressLayerIndex !== -1 ? parseInt(address[addressLayerIndex + 1]) : Number.NaN;
+
+        if (isNaN(channel) || isNaN(layer) || !this.#channels.some(c => c.channel == channel && c.layer == layer)) {
+            return;
+        }
+
+        const currentTime = msg.args[0].value; // Current seconds
+        const totalTime = msg.args[1].value;   // Total seconds
     }
 }
