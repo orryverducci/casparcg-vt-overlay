cf24f0fa4dbf79abf8ac24b2fb692fd491cf5b14
Orry Verducci <orry@orryverducci.co.uk>
2026-06-07 17:21:40 +0100

    Add error handling for OSC server

--- src/caspar-osc.js
+++ src/caspar-osc.js
@@ -24,6 +24,7 @@ export default class CasparOsc {
         this.#server.on('ready', this.serverReady.bind(this));
         this.#server.on('message', this.processMessage.bind(this));
         this.#server.on('bundle', this.processBundle.bind(this));
+        this.#server.on('error', this.logError.bind(this));
     }
 
     serverReady() {
@@ -56,5 +57,9 @@ export default class CasparOsc {
         } else if (address[address.length - 2] === 'file' && address[address.length - 1] === 'name') {
             this.channelStatuses.get(`${channel}-${layer}`).updateName(msg.args[0].value);
         }
+    }
+
+    logError(err) {
+        Logger.error(`Error processing OSC message: ${err}`);
     }
 }
